3996619dda5805870696d1df488c9289132dca4e
Oladimeji Abdrahman Adebayo <abdrahmanoladimeji02@gmail.com>
2023-02-12 13:00:47 +0100

    Update: priority underway

--- src/App.js
+++ src/App.js
@@ -17,10 +17,18 @@ function App() {
   const formRef = useRef();
   const navRef = useRef();
   const [modal, setModal] = useState(false);
+  const [priorityModal, setPriorityModal] = useState(false);
 
-  const handleSubmit = (e) => {
+  const handleSubmitEarly = (e) => {
     e.preventDefault();
     setModal(true)
+    for (let i = 0; i < 2; i++) {
+      formRef.current[i].value = '';
+    }
+  }
+  const handleSubmitPriority = (e) => {
+    e.preventDefault();
+    setPriorityModal(true)
     for (let i = 0; i < 2; i++) {
       formRef.current[i].value = '';
     }
@@ -33,9 +41,8 @@ function App() {
   }
   return (
     <div className="bg-primary text-light">
-      <section id="top" className="min-h-screen">
-        <div className="h-[60px] md:h-[100px] backdrop-blur fixed w-full"></div>
-        <div className="flex justify-between items-center fixed py-auto w-full pr-[5%] pl-[5%] px-0 md:px-[50px] h-fit pt-2 md:h-[100px]">
+        <div className="h-[60px] md:h-[100px] backdrop-blur z-[900] fixed w-full"></div>
+        <div className="flex justify-between items-center fixed z-[999] py-auto w-full pr-[5%] pl-[5%] px-0 md:px-[50px] h-fit pt-2 md:h-[100px]">
           <div className="h-fit">
             <img src={logo} alt="Loyalbaze" className="h-[45px] md:h-[50px]" />
           </div>
@@ -81,9 +88,10 @@ function App() {
             </div>
           </div>
         </div>
-        <div className="h-full mt-0 pt-20 md:pt-[150px] my-10 md:my-0 md:h-[calc(100vh_-_120px)] flex justify-center flex-col">
+      <section id="top" className="min-h-screen">
+        <div className="min-h-screen h-full md:h-[calc(100vh_-_100px)] mt-0 pt-[100px] md:pt-[150px] my-10 md:my-0 flex justify-center flex-col">
           <div className="flex flex-col items-center">
-            <h1 className="text-[48px] md:text-[52px] leading-[65px] font-black text-center w-[90%] max-w-[22ch] md:max-w-[18ch] mb-4">
+            <h1 className="text-[40px] md:text-[52px] leading-[55px] md:leading-[65px] font-black text-center w-[90%] max-w-[22ch] md:max-w-[18ch] mb-4">
               Turn your best customers into{" "}
               <span className="text-[#A75FD2]">Loyal fans</span>
             </h1>
@@ -101,9 +109,9 @@ function App() {
           <form
             ref={formRef}
             className="flex items-center flex-col"
-            onSubmit={handleSubmit}
+            onSubmit={handleSubmitEarly}
           >
-            <div className="flex items-center w-[90%] max-w-[325px] rounded border p-1 mb-4">
+            <div className="flex items-center w-[90%] max-w-[325px] rounded border p-1 mb-6">
               <label htmlFor="name">
                 <HiOutlineUserCircle size={20} className="mx-1" />
               </label>
@@ -116,7 +124,7 @@ function App() {
                 required
               />
             </div>
-            <div className="flex items-center w-[90%] max-w-[325px] rounded border p-1 mb-4">
+            <div className="flex items-center w-[90%] max-w-[325px] rounded border p-1 mb-6">
               <label htmlFor="email">
                 <HiOutlineEnvelope size={20} className="mx-1" />
               </label>
@@ -129,7 +137,7 @@ function App() {
                 required
               />
             </div>
-            <div className="w-[90%] max-w-[325px] mb-4">
+            <div className="w-[90%] max-w-[325px] mb-6">
               <button
                 className="w-[100%] rounded-full bg-gradient-to-r from-secondary to-tertiary py-[4px] hover:scale-95 transition duration-200"
                 type="submit"
@@ -195,10 +203,11 @@ function App() {
           <div className='absolute bottom-16 left-2 md:left-[40%]'>
             <img src={star} alt='Line' className='h-8' />
           </div>
-          <div className='absolute md:-top-[70px] top-[35%] right-[14%]'>
+          <div className='absolute md:-top-[70px] top-[35%] right-[4%] md:right-[14%]'>
             <img src={emptyStar} alt='Line' className='h-7' />
           </div>
-          <div className='absolute circle -bottom-10'></div>
+          <div className='absolute circle bottom-[42rem] md:bottom-[12rem] right-[50%] md:right-[80%]'></div>
+          <div className='absolute circle bottom-[85rem] md:bottom-[52rem] right-[50%]'></div>
           <div className='w-full md:w-1/2 flex justify-center items-center'>
             <div className='mt-6 md:mt-0'>
               <div className='flex justify-center ml-[90%] -mt-10 mb-10 md:-mr-40'>
@@ -220,7 +229,7 @@ function App() {
             </div>
           </div>
           <div className='flex w-full justify-center items-center md:w-1/2'>
-            <div className='bg-[#1D2939] mx-auto w-[85%] max-w-[533px] p-5 md:p-10 rounded-xl'>
+            <form onsubmit={handleSubmitPriority} className='bg-[#1D2939] mx-auto w-[85%] max-w-[533px] p-5 md:p-10 mb-6 md:mb-0 rounded-xl'>
               <h2 className='pb-6'>
                 Book a Consultation with us
               </h2>
@@ -239,7 +248,7 @@ function App() {
               <div className='mt-6'>
                 <textarea placeholder='Drop a message...' className='bg-transparent color-transparent border py-[6px] px-3 min-h-[40px] outline-none focus:border-[#fff] border-[#ddd]/50 rounded-lg w-full' required />
               </div>
-              <div className="mt-6">
+              <div className="mt-6 ">
                 <button
                   className="w-[100%] rounded-full bg-gradient-to-r from-secondary to-tertiary py-[4px] hover:scale-95 transition duration-200"
                   type="submit"
@@ -247,9 +256,40 @@ function App() {
                   Send Request
                 </button>
               </div>
-            </div>
+            </form>
           </div>
         </div>
+        {priorityModal && (
+          <div
+            onClick={closeModal}
+            className="flex justify-center items-center fixed h-screen w-screen top-0 bg-primary/50"
+          >
+            <div className="flex justify-center items-center">
+              <div className="flex flex-col items-center px-4 md:px-8 py-16 bg-[#15141B] rounded-[20px] w-[90%] max-w-[446px] p-4 relative">
+                <div
+                  onClick={closeModal}
+                  className="absolute top-4 right-4 cursor-pointer hover:scale-90 transition duration-200"
+                >
+                  <FaTimes size={25} />
+                </div>
+                <div>
+                  <div className="border-[#ffffff33] border-[10px] mb-8 w-fit rounded-full">
+                    <div className="bg-[#20AC5B] rounded-full p-4 box-shadow">
+                      <ImCheckmark size={40} />
+                    </div>
+                  </div>
+                </div>
+                <div className="text-center">
+                  <h2 className="font-bold text-2xl py-3">Congratulations</h2>
+                  <p className="max-w-[35ch]">
+                    Great move! You're one step closer to getting your hands on
+                    the product. Please check your mail for more information.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </section>
     </div>
   );
