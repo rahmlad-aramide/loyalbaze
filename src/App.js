898a366c313952075a0d3634c2f8849a4bb2bd1c
Oladimeji Abdrahman Adebayo <abdrahmanoladimeji02@gmail.com>
2023-02-12 15:20:54 +0100

    Update: completed early access

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import logo from './Assets/logo.svg';
 import line from './Assets/line.svg';
 import emptyStar from './Assets/empty-star.svg'
 import star from './Assets/filled-star.svg'
+import Matrix from './Assets/matrix.svg'
 
 import {HiOutlineUserCircle} from 'react-icons/hi';
 import {HiOutlineEnvelope} from 'react-icons/hi2';
@@ -92,16 +93,28 @@ function App() {
             </div>
           </div>
         </div>
-      <section id="top" className="min-h-screen">
+      <section id="top" className="min-h-screen relative overflow-x-hidden w-screen">
+        <div className='ellipse1 absolute top-[7rem] left-[5%] z-10'></div>
+        <div className='ellipse2 absolute right-0 top-20 md:top-28 md:right-[20%]'></div>
+        <div className='ellipse3 absolute top-[25rem] -right-[7rem] md:-right-[5%] z-10'></div>
+        <div className='flex absolute top-[8.5rem] right-[62%] z-10 justify-center ml-[30%]'>
+          <img src={star} alt='star' className='h-7' />
+        </div>
+        <div className='flex absolute bottom-[6rem] right-[4%] z-10 justify-center ml-[90%]'>
+          <img src={star} alt='star' className='h-7' />
+        </div>
+        <div className='flex absolute bottom-[3rem] right-[4%] z-10 justify-center mr-[90%]'>
+          <img src={star} alt='star' className='h-7' />
+        </div>
         <div className="min-h-screen h-full md:h-[calc(100vh_-_100px)] mt-0 pt-[100px] md:pt-[150px] my-10 md:my-0 flex justify-center flex-col">
           <div className="flex flex-col items-center">
-            <h1 className="text-[40px] md:text-[52px] leading-[55px] md:leading-[65px] font-black text-center w-[90%] max-w-[22ch] md:max-w-[18ch] mb-4">
+            <h1 className="text-[40px] md:text-[52px] leading-[55px] z-10 md:leading-[65px] font-black text-center w-[90%] max-w-[22ch] md:max-w-[18ch] mb-4">
               Turn your best customers into{" "}
               <span className="text-[#A75FD2]">Loyal fans</span>
             </h1>
             <p
               id="early-access"
-              className="w-[90%] max-w-[60ch] text-center text-lg mb-8"
+              className="w-[90%] max-w-[60ch] z-10 text-center text-lg mb-8"
             >
               Loyalbaze, empowers businesses to offer digital, mobile-first
               loyalty programs to their customers. With our AI Powered platform,
@@ -115,6 +128,7 @@ function App() {
             className="flex items-center flex-col"
             onSubmit={handleSubmitEarly}
           >
+            <div className='static md:absolute md:left-[63%] h-24 -mt-4 md:-mt-7'><img src={Matrix} alt="matrix" className='h-20' /></div>
             <div className="flex items-center w-[90%] max-w-[325px] rounded border p-1 mb-6">
               <label htmlFor="name">
                 <HiOutlineUserCircle size={25} className="mx-1" />
@@ -170,7 +184,7 @@ function App() {
           <Modal setModal={setModal} />
         )}
       </section>
-      <section id="priority-access" className="min-h-screen">
+      <section id="priority-access" className="h-full pb-4 min-h-[calc(100vh_-_150px)]">
         <div className='flex flex-col md:flex-row relative'>
           <div className='absolute ellipse -top-10 left-[15%] mt-5 md:mt-0'></div>
           <div className='absolute -top-6 left-[45%]'>
@@ -206,7 +220,7 @@ function App() {
           </div>
           <div className='flex w-full justify-center items-center md:w-1/2'>
             <form ref={priorityFormRef} onSubmit={handleSubmitPriority} className='bg-[#1D2939] mx-auto w-[85%] max-w-[533px] p-5 md:p-10 mb-6 md:mb-0 rounded-xl'>
-              <h2 className='pb-6'>
+              <h2 className='pb-6 text-lg md:text-2xl font-medium'>
                 Book a Consultation with us
               </h2>
               {consultData.map((item, index)=> (
@@ -224,7 +238,7 @@ function App() {
               <div className='mt-6'>
                 <textarea placeholder='Drop a message...' className='bg-transparent color-transparent border py-[6px] px-3 min-h-[40px] outline-none focus:border-[#fff] border-[#ddd]/50 rounded-lg w-full' required />
               </div>
-              <div className="mt-6 ">
+              <div className="mt-6 mb-2">
                 <button
                   className="w-[100%] rounded-full bg-gradient-to-r from-secondary to-tertiary py-[6px] hover:scale-95 transition duration-200"
                   type="submit"
